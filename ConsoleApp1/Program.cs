d27249dade8a6167367147b9f46777e96647abab
Arturo <Arturo@DESKTOP-KLD43RF>
2025-12-06 10:11:57 -0600

    modifique el program y agregue una clase

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! 2.0");
+
+
+Console.WriteLine("This is an updated version of the console application.");
+
+Console.WriteLine("Goodbye, World!");
+
+Console.WriteLine("This line was added in the latest update.");

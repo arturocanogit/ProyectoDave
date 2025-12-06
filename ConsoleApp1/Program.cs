2437b718892394a49ffd5fd626fc82a8697fa6a7
Arturo <Arturo@DESKTOP-KLD43RF>
2025-12-06 11:04:49 -0600

    Nueva linea en nueva rama dave

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,10 +7,10 @@ Console.WriteLine("This is an updated version of the console application.");
 Console.WriteLine("Goodbye, World!");
 
 Console.WriteLine("This line was added in the latest update.");
+Console.WriteLine("This line was added in the latest update.");
 
 Clase clase = new Clase();
 int resultado = clase.Sumar(5, 7);
 
 Console.WriteLine($"The sum of 5 and 7 is: {resultado}");
 
-    
